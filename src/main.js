2899a4799e70e8201a4e5c75b16d1dd248f16020
Mateusz Haligowski <mhaligowski@gmail.com>
2017-08-22 07:37:31 -0700

    Generate index.js, package.json and webpack

--- src/main.js
+++ src/main.js
@@ -3,29 +3,79 @@ const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
 
-var targetDir;
-
 const program = require('commander');
 
 program.version('0.1.0')
-       .arguments('<dir>')
-       .action(dir => targetDir = dir)
-       .option('-n, --name <name>', 'The name of the project')
-       .parse(process.argv);
+    .arguments('<name> <directory>')
+    .parse(process.argv);
 
-if (typeof targetDir === 'undefined') {
-    console.error(chalk.red('Target directory is required'));
-    process.exit(1);
+if (program.args.length != 2) program.help();
+
+function generateIndexJs(functionName, targetDir) {
+    console.log(chalk.yellow('Generating index.js'));
+
+    fs.mkdirSync(path.resolve(targetDir, 'src'));
+
+    const indexJsTemplate = require('./templates/index.js.hbs');
+    const indexJsPath = path.resolve(targetDir, 'src', 'index.js');
+    const indexJsBody = indexJsTemplate({ name: functionName });
+
+    fs.writeFileSync(indexJsPath, indexJsBody);
+
+    console.log(chalk.green('Generated index.js'));
 }
 
-console.log(chalk.yellow('Creating the src directory'));
-const srcDir = path.resolve(targetDir, 'src');
-fs.mkdirSync(srcDir);
-console.log(chalk.green('src directory created'));
+function generatePackageJson(functionName, targetDir) {
+    console.log(chalk.yellow('Generating package.json'));
 
-// generate index.js
-const indexJsTemplate = require('./templates/index.js.hbs');
+    const packageJson = {
+        "name": functionName,
+        "version": "0.0.1",
+        "main": 'index.js',
+        "devDependencies": {
+            "babel-core": "^6.25.0",
+            "babel-jest": "^20.0.3",
+            "babel-loader": "^7.1.1",
+            "babel-preset-env": "^1.6.0",
+            "generate-package-json-webpack-plugin": "^0.1.2",
+            "jest": "^20.0.4",
+            "regenerator-runtime": "^0.10.5",
+            "webpack": "^3.3.0",
+            "webpack-node-externals": "^1.6.0"
+        },
+        "scripts": {
+            "test": "jest",
+            "build": "webpack --config ./config/webpack.config.js"
+        },
+        "jest": {
+            "testEnvironment": "node"
+        }
+    };
 
-console.log(chalk.yellow('Generating index.js'));
-console.log(indexJsTemplate({ name: program.name }));
+    const packageJsonPath = path.resolve(targetDir, 'package.json');
+    const packageJsonBody = JSON.stringify(packageJson, null, '    ');
+    
+    fs.writeFileSync(packageJsonPath, packageJsonBody);
+
+    console.log(chalk.green('Generated package.json'));
+}
+
+function generateWebpackConfig(functionName, targetDir) {
+    console.log(chalk.yellow('Generating WebPack configuration'));
+
+    fs.mkdirSync(path.resolve(targetDir, 'config'));
+
+    const webpackConfigTemplate = require('./templates/webpack.config.js.hbs');
+    const webpackConfigPath = path.resolve(targetDir, 'config', 'webpack.config.js');
+    const webpackConfigBody = webpackConfigTemplate({ name: functionName });
+
+    fs.writeFileSync(webpackConfigPath, webpackConfigBody);
+
+    console.log(chalk.green('Generated webpack configuration'));
+}
+
+generateIndexJs(...program.args);
+generatePackageJson(...program.args);
+generateWebpackConfig(...program.args);
+
 
